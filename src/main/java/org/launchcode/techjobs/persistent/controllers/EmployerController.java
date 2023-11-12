c59ae084d835b9909993841e9aaeb0ca8f7c54aa
MelanieRussell <140012880+MelanieRussell@users.noreply.github.com>
2023-11-12 17:55:38 -0600

    Update EmployerController.java

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -2,17 +2,31 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.*;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+
+    @Autowired
+   private EmployerRepository employerRepository;
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "All Employers");
+        List<Employer> employers = (List<Employer>) employerRepository.findAll();
+        model.addAttribute("employers", employers );
+        return "employers/index";
+    }
+
+
+
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -25,16 +39,18 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
             return "employers/add";
         }
 
+        employerRepository.save(newEmployer);
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
 
-        Optional optEmployer = null;
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
