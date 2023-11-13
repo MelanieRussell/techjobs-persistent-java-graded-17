04b75afacae0b16f254551968ae1b2171ee389a9
MelanieRussell <140012880+MelanieRussell@users.noreply.github.com>
2023-11-12 18:02:20 -0600

    Update Employer.java

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,35 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @OneToMany
+    @JoinColumn (name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+
+
+    @NotBlank(message="Name is required.")
+    @Size(min= 3, max = 100, message = "Name must be between 3 and 100 characters.")
+    String location;
+
+    public Employer(){
+
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
+
